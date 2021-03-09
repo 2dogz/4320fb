c86ce8788dfd87dc8e38341643ab73af703710e0
2dogz <kylewebb58@yahoo.com>
2021-03-09 11:20:59 -0600

    add hardcoded start,stop values and divisible by values, also fixed spelling

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+# HARD CODED START AND STOP VALUES, PYTHON INDEX STARTS AT 0
+start = 1
+stop = 101
+
+# HARD CODED DIVISIBLE VALUES, ABLE TO CHANGE NUMBERS MUCH FASTER
+divisibleByA = 3
+divisibleByB = 5
+
 #Loops over numbers 1-100
-for x in range(1,101):
-    #modulo operator is useful to check if a variable is divisable by another, if the remainder of the modulo operation is 0, the number is divisable.
-    if x % 3 == 0 and x % 5 == 0:
-        # IF LOOP VAR DIVISABLE BY 3 AND 5 PRINT FIZZBUZZ
+for x in range(start,stop):
+    #modulo operator is useful to check if a variable is divisible by another, if the remainder of the modulo operation is 0, the number is divisible.
+    if x % divisibleByA == 0 and x % divisibleByB == 0:
+        # IF LOOP VAR DIVISIBLE BY 3 AND 5 PRINT FIZZBUZZ
         print('{} fizzbuzz'.format(x))
     else:
-        if x % 3 == 0:
-            # IF LOOP VAR DIVISABLE BY 3 PRINT FIZZ
+        if x % divisibleByA == 0:
+            # IF LOOP VAR DIVISIBLE BY 3 PRINT FIZZ
             print('{} fizz'.format(x))
-        elif x % 5 == 0:
-            # IF LOOP VAR DIVISABLE BY 3 PRINT BUZZ
+        elif x % divisibleByB == 0:
+            # IF LOOP VAR DIVISIBLE BY 3 PRINT BUZZ
             print('{} buzz'.format(x))
         else:
-            # IF LOOP VAR IS NOT DIVISABLE BY 3 OR 5 OR BOTH PRINT THE LOOP VARIABLE
+            # IF LOOP VAR IS NOT DIVISIBLE BY 3 OR 5 OR BOTH PRINT THE LOOP VARIABLE
             print(x)
